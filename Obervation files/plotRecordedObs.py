d4c91eee595641b29176b46414ffb3a34d6ec65c
mariahotoiu <mariahotoiu@yahoo.com>
2018-06-06 20:08:32 +0200

    Obs files + plots

---
+++ Obervation files/plotRecordedObs.py
@@ -0,0 +1,91 @@
+##############################################################
+#                                                            #
+#    Mark Hoogendoorn and Burkhardt Funk (2017)              #
+#    Machine Learning for the Quantified Self                #
+#    Springer                                                #
+#    Chapter 2                                               #
+#                                                            #
+##############################################################
+
+
+dataset_path = 'D:\Cursuri Master\Machine Learning for the Quantified Self\Recorded obs/'
+result_dataset_path = dataset_path
+
+# Import the relevant classes.
+
+from Chapter2.CreateDataset import CreateDataset
+from util.VisualizeDataset import VisualizeDataset
+from util import util
+import copy
+import os
+
+
+if not os.path.exists(result_dataset_path):
+    print('Creating result directory: ' + result_dataset_path)
+    os.makedirs(result_dataset_path)
+
+# Chapter 2: Initial exploration of the dataset.
+
+# Set a granularity (i.e. how big are our discrete time steps). We start very
+# coarse grained, namely one measurement per minute, and secondly use four measurements
+# per second
+
+granularities = [60000, 250]
+datasets = []
+
+for milliseconds_per_instance in granularities:
+
+    # Create an initial dataset object with the base directory for our data and a granularity
+    DataSet = CreateDataset(dataset_path, milliseconds_per_instance)
+
+    # Add the selected measurements to it.
+
+    # We add the accelerometer data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values/
+    DataSet.add_numerical_dataset('metro-acc.csv', 'timestamps', ['x','y','z'], 'avg', 'acc_phone_')
+    
+    # We add the gyroscope data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values/
+    DataSet.add_numerical_dataset('metro-gyr.csv', 'timestamps', ['x','y','z'], 'avg', 'gyr_phone_')
+   
+    # We add the heart rate (continuous numerical measurements) and aggregate by averaging again
+   # DataSet.add_numerical_dataset('heart_rate_smartwatch.csv', 'timestamps', ['rate'], 'avg', 'hr_watch_')
+
+    # We add the labels provided by the users. These are categorical events that might overlap. We add them
+    # as binary attributes (i.e. add a one to the attribute representing the specific value for the label if it
+    # occurs within an interval).
+    DataSet.add_event_dataset('labels.csv', 'label_start', 'label_end', 'label', 'binary')
+
+    # We add the amount of light sensed by the phone (continuous numerical measurements) and aggregate by averaging again
+    
+    # We add the magnetometer data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values
+    DataSet.add_numerical_dataset('metro-grv.csv', 'timestamps', ['x','y','z'], 'avg', 'mag_phone_')
+    
+    # We add the pressure sensed by the phone (continuous numerical measurements) and aggregate by averaging again
+    
+    # Get the resulting pandas data table
+
+    dataset = DataSet.data_table
+
+    # Plot the data
+
+    DataViz = VisualizeDataset()
+
+    # Boxplot
+    DataViz.plot_dataset_boxplot(dataset, ['acc_phone_x','acc_phone_y','acc_phone_z'])
+
+    # Plot all data
+    DataViz.plot_dataset(dataset, ['acc_', 'gyr_',  'mag_', 'label'], ['like', 'like', 'like', 'like'], ['line', 'line', 'line', 'points'])
+
+    # And print a summary of the dataset
+
+    util.print_statistics(dataset)
+    datasets.append(copy.deepcopy(dataset))
+
+# And print the table that has been included in the book
+
+util.print_latex_table_statistics_two_datasets(datasets[0], datasets[1])
+
+# Finally, store the last dataset we have generated (250 ms).
+#dataset.to_csv('chapter2_result.csv')
